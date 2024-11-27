7c9e91d716f15683d6670ad8a088c81d9889db80
Kien Ta Quang <quangkienta3@gmail.com>
2024-11-27 08:52:04 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended: false}));
 const cors = require("cors");
 app.use(cors());
 
-app.use("/api", require("./routes"));
+app.use("/api", require("./routes")); 
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
